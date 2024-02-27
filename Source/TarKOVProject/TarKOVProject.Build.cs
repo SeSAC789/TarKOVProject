9fb0742b6ea923faa117e1d8d4127181517d6a40
NIJJY7 <102662924+NIJJY7@users.noreply.github.com>
2024-02-27 17:05:53 +0900

    Initial commit

---
+++ Source/TarKOVProject/TarKOVProject.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class TarKOVProject : ModuleRules
+{
+	public TarKOVProject(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+	}
+}
---
+++ Source/TarKOVProject/public/TarKOVProjectGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TarKOVProjectGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ATarKOVProjectGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ATarKOVProjectGameMode();
+};
+
+
+
