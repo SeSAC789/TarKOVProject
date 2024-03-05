13158e783a53577a72882d85630070e4ecaf607c
lsh210726 <136397554+lsh210726@users.noreply.github.com>
2024-03-05 17:35:40 +0900

    클래스 생성

---
+++ Source/TarKOVProject/Public/LSH/ColorSlateBrushAsset.h
@@ -0,0 +1,34 @@
+﻿//// Fill out your copyright notice in the Description page of Project Settings.
+//
+//#pragma once
+//
+//#include "CoreMinimal.h"
+//#include "Slate/SlateBrushAsset.h"
+//#include "ColorSlateBrushAsset.generated.h"
+//
+///**
+// * 
+// */
+//UCLASS()
+//class TARKOVPROJECT_API UColorSlateBrushAsset : public USlateBrushAsset
+//{
+//	GENERATED_BODY()
+//	
+//};
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "Slate/SlateBrushAsset.h"
+#include "ColorSlateBrushAsset.generated.h"
+
+UCLASS()
+class TARKOVPROJECT_API UColorSlateBrushAsset : public UObject
+{
+    GENERATED_BODY()
+
+public:
+    UPROPERTY( EditAnywhere , BlueprintReadWrite , Category = "Brush" )
+    FSlateBrush Brush;
+};
---
+++ Source/TarKOVProject/Public/LSH/InvnetoryWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "InvnetoryWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TARKOVPROJECT_API UInvnetoryWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
